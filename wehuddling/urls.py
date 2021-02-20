c331e922443db1d127f084674bd79d23534f7457
yj-leee <yj.leee1010@gmail.com>
2021-02-20 18:35:54 +0900

    MODIFY: urls.py

--- wehuddling/urls.py
+++ wehuddling/urls.py
@@ -14,8 +14,30 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
+from django.conf.urls import url
+
+from rest_framework import permissions
+from drf_yasg.views import get_schema_view
+from drf_yasg import openapi
+
+schema_view = get_schema_view(
+    openapi.Info(
+        title='wehuddling',
+        default_version='v1',
+        description='wehuddling project API',
+        contact=openapi.Contact(email='yj.leee@google.com')
+    ),
+    public=True,
+    permission_classes=(permissions.AllowAny,)
+)
+
+
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
+    url(r'^swagger(?P<format>\.json|\.yaml)$', schema_view.without_ui(cache_timeout=0), name='schema-json'),
+    url(r'^swagger/$', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
+    url(r'^redoc/$', schema_view.with_ui('redoc', cache_timeout=0), name='schema-redoc'),
+    path('admin', admin.site.urls),
+    path('user', include('user.urls')),
 ]
