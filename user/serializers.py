ef02cc3b48726c993b50e4fefabdf1d788c88fbd
yj-leee <yj.leee1010@gmail.com>
2021-02-21 17:40:13 +0900

    ADD: user.view

--- user/serializers.py
+++ user/serializers.py
@@ -1,39 +1,55 @@
 from rest_framework import serializers
 from rest_framework.validators import UniqueValidator
+from rest_framework_jwt.settings import api_settings
+
+from django.contrib.auth import get_user_model
+from django.contrib.auth.models import update_last_login
+from django.contrib.auth import authenticate
+
 from .models import User
 
+# JWT
+JWT_PAYLOAD_HANDLER = api_settings.JWT_PAYLOAD_HANDLER
+JWT_ENCODE_HANDLER = api_settings.JWT_ENCODE_HANDLER
 
-#class UserSerializer(serializers.Serializer):
-#    class Meta:
-#        id = serializers.IntegerField(read_only=True)
-#        email = serializers.EmailField()
-#        password = serializers.CharField()
-#        name = serializers.CharField()
-#        phone_number = serializers.CharField()
-#        gender = serializers.CharField()
-#        address = serializers.CharField()
-#        is_deleted = serializers.BooleanField()
-#        create_at = serializers.DateTimeField(read_only=True)
-#        update_at = serializers.DateTimeField(read_only=True)
+User = get_user_model()
+
 
 class UserSerializer(serializers.ModelSerializer):
     email = serializers.EmailField(
             required=True,
             validators=[UniqueValidator(queryset=User.objects.all())]
             )
-    password = serializers.CharField(min_length=8)
-    name = serializers.CharField()
-    phone_number = serializers.CharField()
-    gender = serializers.CharField(required=False,allow_blank=True)
-    address = serializers.CharField(required=False,allow_blank=True)
+    gender = serializers.CharField(allow_blank=True)
+    address = serializers.CharField(allow_blank=True)
 
     class Meta:
         model = User
         fields = ('email', 'password', 'name', 'phone_number', 'gender', 'address')
 
+class UserLoginSerializer(serializers.Serializer):
+    email = serializers.CharField(max_length=100)
+    password = serializers.CharField(max_length=128)
+    token = serializers.CharField(max_length=255, read_only=True)
 
+    def validate(self, data):
+        username = data.get('email')
+        password = data.get('password')
 
+        if user is None:
+            return {'email':'None'}
 
+        try:
+            payload = JWT_PAYLOAD_HANDLER(user)
+            jwt_token = JWT_ENCODE_HANDLER(payload)
+            update_last_lognin(None, user)
 
+        except User.DoesNontExist:
+            raise serializers.ValidationError('User does not exist')
+
+        return {
+            'email' : user.eamil,
+            'token' : jwt_token
+        }
 
 
--- user/urls.py
+++ user/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path
-from .views import UserCreate
+from .views import UserCreate, UserLogin
 
 
 urlpatterns = [
-    path('/signup', UserCreate.as_view(), name='signup')
+    path('/signup', UserCreate.as_view(), name='signup'),
+    path('/signin', UserLogin.as_view(), name='signin'),
 ]
 
