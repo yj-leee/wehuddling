6a11b33da0fccb12f7f1733243ca05165684329f
yj-leee <yj.leee1010@gmail.com>
2021-03-03 23:44:46 +0900

    user Login

--- user/views.py
+++ user/views.py
@@ -1,16 +1,16 @@
-from django.shortcuts import get_object_or_404, render
-from django.contrib.auth import authenticate
-from django.contrib.auth.hashers import check_password, make_password
+from django.shortcuts                import get_object_or_404, render
+from django.contrib.auth             import authenticate
+from django.contrib.auth.hashers     import check_password, make_password
 
-from rest_framework import status
-from rest_framework.views import APIView
-from rest_framework.response import Response
+from rest_framework                  import status
+from rest_framework.views            import APIView
+from rest_framework.response         import Response
 from rest_framework.authtoken.models import Token
 
-from drf_yasg.utils import swagger_auto_schema
+from drf_yasg.utils                  import swagger_auto_schema
 
-from .models import User
-from .serializers import UserSerializer, UserLoginSerializer
+from .models                         import User
+from .serializers                    import UserSerializer, UserLoginSerializer
 
 class UserCreate(APIView):
     def post(self, request):
@@ -19,8 +19,8 @@ class UserCreate(APIView):
         if serializer.is_valid():
             password = serializer.validated_data['password']
             serializer.validated_data['password'] = make_password(password)
-            user = serializer.save()
-            token = Token.objects.create(user=user)
+            user     = serializer.save()
+            token    = Token.objects.create(user=user)
 
             if user:
                 return Response({"token":token.key}, status=status.HTTP_201_CREATED)
@@ -31,9 +31,9 @@ class UserLogin(APIView):
         serializer = UserLoginSerializer(data=request.data)
 
         if serializer.is_valid():
-            email = serializer.validated_data['email']
+            email    = serializer.validated_data['email']
             password = serializer.validated_data['password']
-            user = authenticate(email=email, password=password)
+            user     = authenticate(email=email, password=password)
 
             if user:
                 token = Token.objects.get(user=user)
